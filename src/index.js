9d5ad4bc8fd38f30685542df3aa0ea174069cdb1
Karol Trombala <ktrombala@gmail.com>
2023-07-12 12:20:30 +0200

    assigned vars

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import Notiflix from 'notiflix';
 axios.defaults.headers.common['x-api-key'] =
   'live_ll4GIjvo9ClYsMVe1gwe9R5jiQQNgNMPwreF7lvehA0lqTrAYQERaVkPMedGtuFB';
 
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo =document.querySelector('.cat-info');
+
