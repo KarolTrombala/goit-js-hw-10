23fd430f4fc09eac9bd835ba70a569feaa1732df
Karol Trombala <ktrombala@gmail.com>
2023-07-12 15:38:17 +0200

    added few functions

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,22 @@
 import axios from "axios";
+import Notiflix from "notiflix";
 
 export function fetchBreeds() {
     return axios
     .get('https://api.thecatapi.com/v1/breeds')
     .then(response => response.data)
     .catch(error => {
-        console.error('Błąd podczas pobierania kolekcji ras', error);
+        Notiflix.Notify.failure('Błąd podczas pobierania kolekcji ras:');
+        throw error;
+    });
+}
+
+function fetchCatByBreed(breedId) {
+    return axios
+    .get('https://api.thecatapi.com/v1/images/search?breed_ids=${breedId')
+    .then(response => response.data)
+    .catch(error => {
+        Notiflix.Notify.failure('Błąd podczas pobierania informacji o kocie:');
         throw error;
     });
 }
--- src/index.js
+++ src/index.js
@@ -8,5 +8,46 @@ axios.defaults.headers.common['x-api-key'] =
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
-const catInfo =document.querySelector('.cat-info');
+const catInfo = document.querySelector('.cat-info');
 
+breedSelect.addEventListener('change', selectCat);
+
+function selectCat (e) {
+    const breedId = e.target.value;
+    if(breedId) {
+        loader.style.display = 'block';
+        error.style.display = 'none';
+    }
+}
+
+function fetchCat (breedId) {
+    axios
+    .get('https://api.thecatapi.com/v1/images/search?breed_ids=${breedId')
+   .then(response => {
+    const catItemInfo = response.data[0];
+    showCat (catItemInfo)
+    })
+    .catch(error => {
+        console.error(error);
+      showError();
+    }) 
+    .finally(() => {
+        loader.style.display = 'none';
+    }) ;
+}
+
+function showCat (catItemInfo) {
+    const { name, description, temperament } = catInfo.breeds[0];
+  const { url } = catInfo;
+  const catInfoHTML = `
+    <div>
+    <img  src="${url}" alt="">
+      <h2>${name}</h2>
+      <p><strong>Description:</strong> ${description}</p>
+      <p><strong>Temperament:</strong> ${temperament}</p>
+    </div>
+  `;
+
+  catInfo.innerHTML = catInfoHTML;
+  catInfo.style.display = 'block';
+}
